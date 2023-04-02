25da0a86dde57217a03642ac59806faa4f06e4e8
Anna Artemova <annartmva@gmail.com>
2023-04-02 21:59:23 +0400

    Add fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    fontFamily: {
+      'sans': ['"Barlow Condensed"', 'sans-serif'],
+      'body': ['Barlow', 'sans-serif'],
+    },
     extend: {},
   },
   plugins: [],
